4a6660a15d6e5d7bd2cfcc0267d1aa3d2ed27fcb
Abhishek Anand <abhishek.anand@clipboardhealth.com>
2025-05-15 12:47:00 +0530

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import type React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import type React from "react"
 import "./globals.css"
-
-import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -23,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
